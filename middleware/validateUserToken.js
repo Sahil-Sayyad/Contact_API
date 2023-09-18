1ac043cde5d4a528632b790472bc8277cb3d4edb
Backend Engineer <96423459+Sahil-Sayyad@users.noreply.github.com>
2023-09-18 20:48:57 +0530

    Update validateUserToken.js

--- middleware/validateUserToken.js
+++ middleware/validateUserToken.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const validateToken = asyncHandler(async (req, res, next) => {
   let token;
 
-  let authHeader = req.headers.authorization || req.headers.Authorization;
+  let authHeader = req.headers.authorization ;
 
   if (authHeader && authHeader.startsWith("Bearer")) {
     token = authHeader.split(" ")[1];
